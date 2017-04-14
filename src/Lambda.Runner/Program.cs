0d86464cfd9d8a9abd1cf8a2e9a24eb623c80988
jincod <vadim.abdrashitov@gmail.com>
2017-04-14 14:46:01 +0500

    init

---
+++ src/Lambda.Runner/Program.cs
@@ -0,0 +1,13 @@
+﻿using SimpleLambda;
+
+namespace Lambda.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var functions = new Functions();
+            functions.Process(null, null);
+        }
+    }
+}
---
+++ src/SimpleLambda/Functions.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace SimpleLambda
+{
+    public class Functions
+    {
+        public static void Main()
+        {
+            
+        }
+        public APIGatewayProxyResponse Process(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            return new APIGatewayProxyResponse();
+        }
+    }
+}
